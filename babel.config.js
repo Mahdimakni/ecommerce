b58bef935ace8a88f6c511e8615a5c8a4e19ab59
mahdi <maknimahdi6@gmail.com>
2025-05-06 17:50:11 +0100

    test20

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
-module.exports = function (api) {
-    api.cache(true);
-  
-    return {
-      presets: [
-        '@babel/preset-env',
-        '@babel/preset-react'
-      ],
-    };
-  };
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+};
   
