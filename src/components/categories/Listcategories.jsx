8ca2636c684d89e3d2048afaa247970f87cd8453
mahdi <maknimahdi6@gmail.com>
2025-06-04 10:05:06 +0100

    54

--- src/components/categories/Listcategories.jsx
+++ src/components/categories/Listcategories.jsx
@@ -1,27 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Listcategories = () => {
   const [categories, setCategories] = useState([]);
 
-  const fetchcategories = async () => {
-    const res = await axios.get("http://localhost:3001/api/categories");
-    setCategories(res.data);
-  };
-
   useEffect(() => {
-    fetchcategories();
+    // Simuler des données statiques (comme une "base de données locale")
+    const fakeData = [
+      {
+        _id: '1',
+        nomcategorie: 'Électronique',
+        imagecategorie: 'https://img.grouponcdn.com/deal/49Js9bbWtLM2dcwJYrZEghwLKhg2/49-1000x600/v1/t600x362.jpg',
+      },
+      {
+        _id: '2',
+        nomcategorie: 'Vêtements',
+        imagecategorie: 'https://img.freepik.com/photos-gratuite/boutique-vetements-boutique-vetements-cintre-boutique-moderne_1150-8886.jpg',
+      },
+      {
+        _id: '3',
+        nomcategorie: 'sante',
+        imagecategorie: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO2pQ47EyTXEwv2rDcRmurv9VgbqtDPrlL7Q&s',
+      },
+      
+      {
+        _id: '4',
+        nomcategorie: 'mecanique',
+        imagecategorie: 'https://img.freepik.com/vecteurs-libre/concept-rond-outils-garage-vintage-monochrome_1284-39202.jpg?semt=ais_items_boosted&w=740',
+      },
+      {
+        _id: '5',
+        nomcategorie: 'entrainement',
+        imagecategorie: 'https://www.placedupro.com/photos/boutique/produits/zooms/11658086-1-600_10799.jpg',
+      },
+      {
+        _id: '6',
+        nomcategorie: 'nuttrition',
+        imagecategorie: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0xnRLdMJ5PRE57mkkmc1LGlkzdDgFbxG1ug&s',
+      },
+      {
+        _id: '7',
+        nomcategorie: 'Maison',
+        imagecategorie: 'https://www.maisons-mca.com/wp-content/uploads/2024/01/les-bonnes-raisons-craquer-plan-maison-contemporaine-1.jpg',
+      },
+    ];
+    setCategories(fakeData);
   }, []);
 
-  const handleDelete = async (id) => {
+  const handleDelete = (id) => {
     if (window.confirm("Êtes-vous sûr de vouloir supprimer ?")) {
-      try {
-        await axios.delete(`http://localhost:3001/api/categories/${id}`);
-        fetchcategories();
-      } catch (error) {
-        console.log(error);
-      }
+      // Supprimer localement (sans backend)
+      const filtered = categories.filter(cat => cat._id !== id);
+      setCategories(filtered);
     }
   };
 
